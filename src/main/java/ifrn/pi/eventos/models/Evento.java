eb5b26bddfb14391ae75131fdb3a101a9bd58955
ISMAEL AQUILLA SALVIANO DA SILVA <20221204010034@ifrn.local>
2026-09-01 14:21:46 -0300

    Enviando dados do formulario para a aplicação

--- src/main/java/ifrn/pi/eventos/models/Evento.java
+++ src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -55,4 +55,13 @@ public class Evento {
     public void setHorario(String horario) {
         this.horario = horario;
     }
+
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", nome=" + nome + ", local=" + local + ", data=" + data + ", horario=" + horario
+				+ "]";
+	}
+    
+    
+    
 }
