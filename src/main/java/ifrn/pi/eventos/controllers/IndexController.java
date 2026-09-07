28494d8d23d986c085295934db56e2aaff37170d
ismael <ismael.a@escolar.ifrn.edu.br>
2026-09-06 22:51:20 -0300

    Atualizando layout da aplicação

--- src/main/java/ifrn/pi/eventos/controllers/IndexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-	
+
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("Chamou o método index");
