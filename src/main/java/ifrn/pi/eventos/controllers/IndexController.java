a739d50f8d1e8ca907056c550fe0de2d9abe125f
ismael <ismael.a@escolar.ifrn.edu.br>
2026-09-06 23:48:02 -0300

    Criando a listagem de eventos

--- src/main/java/ifrn/pi/eventos/controllers/IndexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/IndexController.java
@@ -9,6 +9,6 @@ public class IndexController {
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("Chamou o método index");
-		return "home";
+		return "redirect:/eventos";
 	}
 }
